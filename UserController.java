cbada648319a730f4e4ffcb2a7e0a0c52c4929e9
gim159 <gim159@naver.com>
2021-03-17 12:07:20 +0900

    Add UserController[update]

--- UserController.java
+++ UserController.java
@@ -19,4 +19,20 @@ public class UserController {
 	 return "user/updata";
 	}
 	
+	// 회원 수정 페이지로 이동
+	@RequestMapping("/user/modify.do")
+	public String modify() {
+	  logger.info("경로: modify")
+	 return "user/ modifys";
+	}
+
+
+`	==== ㅇㅁㄴㅇㄹㄴㄻㄻㅇㄴㄴㅇㅇㄴㄹ
+	// 회원 삭제 페이지로 이동 -[마스터 브런치 추가]
+	@RequestMapping("/user/delete.do")
+	public String modify() {
+	  logger.info("경로: delete")
+	 return "user/ delete";
+	}
+	
 }
